2397b619868c8d0b619e604432b602b9b8d518dc
Aya Adel Hussien <159789859+AyaAdel11@users.noreply.github.com>
2026-02-13 00:37:31 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,4 @@
 def call(String imageName) {
     echo "--- Stage 3: Building Docker Image: ${imageName} ---"
-    sh "docker rmi ${IMAGE_NAME}:latest || true"
-    sh "docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_NAME}:latest"
+    sh "docker build -t ${imageName} ."
 }
